d7b8b6e2b604d2b196f14aceb35967fa894f8db1
Philipp Vincent Ebert <philipp@ebertonline.eu>
2023-03-11 10:55:19 +0100

    Added plugins: leap.nvim, nvim-surround, crates.nvim, rust-tools.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,46 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      require('leap').add_default_mappings()
+    end,
+  },
+  {
+    "kylechui/nvim-surround",
+    lazy = false,
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    tag = "v0.3.0",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function() require("crates").setup() end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    after = { "mason-lspconfig.nvim" },
+    -- Is configured via the server_registration_override installed below!
+    config = function()
+      local rt = require "rust-tools"
+
+      rt.setup {
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<Leader>k", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+          astronvim.lsp.server_settings "rust_analyzer", -- get the server-settings from the AstroNvim tables to allow use with lsp.server-settings and lsp.on_attach user configuration
+        },
+      }
+    end,
+  },
 }
